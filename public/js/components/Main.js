4afa22206d2155e15b9c116af08f9589d11e6eba
Richard Torruellas <rtorruellas@gmail.com>
2015-01-18 12:42:17 -0700

    updating eslint

---
+++ public/js/components/Main.js
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+/* globals React */
+'use strict';
+
+var Main = React.createClass({
+
+  propTypes: {
+    //scroll: React.PropTypes.bool
+  },
+
+  getInitialState: function () {
+    return {};
+  },
+
+  componentDidMount: function () {
+  },
+
+  componentWillUnmount: function () {
+  },
+
+  render: function () {
+    return (
+      <div> Hello Main </div>
+    );
+  }
+});
+
+module.exports = Main;
